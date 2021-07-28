bfb92d0cf7b6692547eeebd5c54c576c859e1810
Константин Лущик <viskuzi1987@gmail.com>
2021-07-29 02:16:21 +0300

    step 4, part 1: add .eslintrc.yml,add  make lint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => { 
+const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hi ' + userName + '!');
+  console.log(`Hi ${userName}!`);
 };
 
 export default getUserName;
