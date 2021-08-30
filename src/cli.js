25db17705c6ff9cccda117043e28e7c42707d93e
Константин Лущик <viskuzi1987@gmail.com>
2021-08-30 17:51:07 +0300

    changed Hi to Hello in brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default getUserName;
