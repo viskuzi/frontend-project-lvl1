c87de770c8ca1c3aaa309b16d4e78cfb8bc45654
Константин Лущик <viskuzi1987@gmail.com>
2021-08-30 17:43:12 +0300

    changed Hi to Hello in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const engine = (rules, getAnswer, checkAnswer) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 
   console.log(rules);
   let gameNumber = 0;
